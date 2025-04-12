3571721666c2491431add01ae1796fc5c16f1043
Muhammad Hamail Khan <mhamailkhandev@gmail.com>
2025-04-12 16:18:25 -0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://hamaildev.vercel.app',
+      url: 'https://hamail.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
-      images: ['https://hamaildev.vercel.app/profile-image.png'],
+      images: ['https://hamail.vercel.app/profile-image.png'],
     },
   ]
 }
